ace5498bb0f7931f40d1c5a27f3e0656c8a9a8a5
Hamza Asif <99635901+hamzaasifbhatti@users.noreply.github.com>
2022-02-19 07:25:35 -0800

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,143 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+int main() {
+                             //size of evaluate size of varriable	
+	/*int a;
+	
+	double d;*/
+
+	/*printf("size of int is %ld",sizeof(a));*/           //sizeof int(4)
+
+	/*printf("size of double is %ld",sizeof(d));*/        //size of double (8)
+
+	//return 0;
+
+	/*char b;
+
+	printf("size of float is %ld",sizeof(b));             //size of char(1)
+
+	getch();*/
+
+	//float c;
+	//printf("size of float is %ld",sizeof(c));           //size of float (4)
+	//getch();
+
+
+	// bolean operation
+
+	/*bool a = true;
+	bool b = true;
+	printf("machine is working:%d",a);
+	printf("machine is not working:%d",b);
+
+	getch();*/
+
+      //if & else condition
+
+
+
+	/*int a = 10;
+	int b = 20;
+
+	if (a>b){
+
+		printf("a is greater then b");
+	}else{
+
+		printf("b is greater then a");
+	}
+	getch();*/
+
+
+
+	/*int a = 10;
+	int b = 20;
+	int c = 30;
+
+	if (a > b && a > c) {
+
+		printf("a is greatest");
+	}
+	else if (b > a && b > c) {
+		printf("b is greatest");
+
+
+	}
+	else {
+		printf("c is greatest");
+	}
+	return 0;*/
+
+	//switch case
+
+	/*int e = 30;
+
+		switch (e) {
+		case 10: printf("value is 10");
+			break;
+
+		case 20: printf("value is 20");
+			break;
+
+		case 30: printf("value is 30");
+			break;
+
+	}
+		return 0;*/
+
+
+	//loops
+
+	// i) for loop
+    
+	/*int i = 0;
+	for (i = 0; i < 11; i++) {
+		printf("%d \n", i);
+	}
+	getch();*/
+
+	// i) while loop
+
+	/*int i = 0;
+	while (i < 5) {
+		printf("%d", i);
+		i++;
+	}
+	getch();*/
+
+      //stored data in C
+
+//int a = 10;
+//printf("%d", a);
+//return 0;
+
+//int a = 20;
+//printf("%d", a);
+//return 0;
+
+//int a = 30;
+//printf("%d", a);
+//return 0;
+
+//char a = 'z';
+//printf("%c", a);
+//return 0;
+
+//float a = 10;
+//printf("%f", a);
+//return 0;
+
+
+//Array in C
+
+//int a [5] = { 1, 2, 3, 4, 5 };
+//for (int i = 0; i < 5; i++) {
+//	printf("%d ", a[i]);
+//
+//}
+
+
+
+}
