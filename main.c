ce0401c768a242e8080e0db7ff3961668d9ace7a
Hamza Asif <99635901+hamzaasifbhatti@users.noreply.github.com>
2022-02-22 04:32:36 -0800

    array

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+int a[3][3][3];
+int i,j,k;
+int n=1;
+for(i=0;i<3;j++){
+    for(j=0;j<3;j++){
+        for(k=0;k<3;k++){
+            a[i][j][k]=n;
+            n++;
+        }
+        }
+    }
+
+ for(i=0;i<3;i++){
+    for(j=0;j<3;j++){
+        for(k=0;k<3;k++){
+            printf("%d ",a[i][j][k]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+ }
+}
