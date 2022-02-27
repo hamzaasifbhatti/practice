9f803b3dd79b311aa3dba420d44d7a206ec42994
Hamza Asif <99635901+hamzaasifbhatti@users.noreply.github.com>
2022-02-27 06:52:39 -0800

    practice 1

---
+++ script.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a;
+
+    // 	double d;
+
+    // 	printf("size of int is %ld",sizeof(a));
+
+    // 	printf("size of double is %ld",sizeof(d));
+
+    // 	char b;
+
+    // 	printf("size of float is %c",sizeof(b));
+
+    // 	getch();
+
+    // 	float c;
+    // 	printf("size of float is %ld",sizeof(c));
+
+    // 	getch();
+    // arithmatics operation
+    // int a, b;
+    // a = 10;
+    // b = 20;
+
+    // printf("a + b = %d\n", a + b);
+    // printf("a + b = %d\n", a - b);
+    // printf("a + b = %d\n", a * b);
+    // printf("a + b = %d\n", a / b);
+
+    // return 0;
+    //&& operation
+    // int a, b;
+    // a = 1;
+    // b = 0;
+
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+
+    //  return 0;
+
+    //  int a, b;
+    // a = 0;
+    // b = 1;
+
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+
+    //  return 0;
+
+    //  int a, b;
+    // a = 0;
+    // b = 0;
+
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+
+    //  return 0;
+
+    //  int a, b;
+    // a = 1;
+    // b = 1;
+
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+
+    //  return 0;
+
+    // || operator
+    // int a, b;
+    // a = 1;
+    // b = 0;
+
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+
+    // return 0;
+
+    //   int a, b;
+    // a = 0;
+    // b = 1;
+
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+
+    // return 0;
+
+    //   int a, b;
+    // a = 0;
+    // b = 0;
+
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+
+    // return 0;
+
+    // int a, b;
+    // a = 1;
+    // b = 1;
+
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+    // printf("a + b = %d\n", a || b);
+
+    // return 0;
+
+    // ! operator
+
+    // int a, b;
+    // a = 1;
+    // b = 0;
+
+    // printf("a + b = %d\n", a && b);
+    // printf("a + b = %d\n", a && b);
+
+    // return 0;
+
+    // relational operator
+
+    // int a, b;
+    // a = 0;
+    // b = 1;
+
+    // printf("a + b = %d\n", a == b);
+    // printf("a + b = %d\n", a != b);
+    // printf("a + b = %d\n", a > b);
+    // printf("a + b = %d\n", a < b);
+    // printf("a + b = %d\n", a >= b);
+    // printf("a + b = %d\n", a <= b);
+
+    // return 0;
+
+    // paractice
+
+    // int a, b;
+    // a = 6;
+    // b = 1;
+
+    // printf("6 x 1 = %d\n", a * b);
+    // a = 6;
+    // b = 2;
+    // printf("6 x 2 = %d\n", a * b);
+    // a = 6;
+    // b = 3;
+    // printf("6 x 3 = %d\n", a * b);
+    // a = 6;
+    // b = 4;
+    // printf("6 x 4 = %d\n", a * b);
+    // a = 6;
+    // b = 5;
+    // printf("6 x 5 = %d\n", a * b);
+    // a = 6;
+    // b = 6;
+    // printf("6 x 6 = %d\n", a * b);
+    // a = 6;
+    // b = 7;
+    // printf("6 x 7 = %d\n", a * b);
+    // a = 6;
+    // b = 8;
+    // printf("6 x 8 = %d\n", a * b);
+    // a = 6;
+    // b = 9;
+    // printf("6 x 9 = %d\n", a * b);
+    // a = 6;
+    // b = 10;
+    // printf("6 x 10 = %d\n", a * b);
+
+    // return 0;
+}
