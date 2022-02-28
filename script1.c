07b6ec441b9733c5baf9c5bfd570f88b037a3ed2
Hamza Asif <99635901+hamzaasifbhatti@users.noreply.github.com>
2022-02-28 04:38:34 -0800

    practice 2

---
+++ script1.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+int main()
+{
+    // Formate Specifier
+
+    // int a = 10;
+    // float b = 20;
+    // printf("this is a %d and this is a %f \n", a, b);
+    // return 0;
+
+    // char a = 'z';
+    // double b = 20;
+    // printf("this is a %c and this is a %lf", a, b);
+    // return 0;
+
+    // Constant in C
+
+    // int a = 10;
+    // const float b = 20;
+    // b = 30;
+    // printf("this value is %d and this value is not change %f", a.b);
+    // return 0;
+
+    // if and else Condition
+
+    // int a = 10;
+    // int b = 20;
+
+    // if (a < b)
+    // {
+    //     printf("a is greater then b %d", a < b);
+    // }
+    // return 0;
+
+    // int a = 10;
+    // int b = 20;
+
+    // if (a > b)
+    // {
+    //     printf("a is greater then b %d", a > b);
+    // }
+    // else
+    // {
+    //     printf("b is greater then a %d", a < b);
+    // }
+    // return 0;
+
+    // char a = 'working';
+    // char b = 'notWorking';
+
+    // if (a > b)
+    // {
+    //     printf("machine is working %c", a > b);
+    // }
+    // else
+    // {
+    //     printf("machine not working %c", a < b);
+    // }
+    // return 0;
+
+    // Switch Case
+
+    // int age;
+    // printf("enter your age \n");
+    // scanf("%d", &age);
+
+    // switch (age)
+    // {
+    // case 3:
+    //     printf("my age is 3");
+    //     break;
+    // case 18:
+    //     printf("my age is 18");
+    //     break;
+    // case 23:
+    //     printf("my age is 23");
+    //     break;
+
+    // default:
+    //     printf("the age is not 3,18,& 23");
+    // }
+    // return 0;
+}
